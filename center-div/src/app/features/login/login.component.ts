664e2d9cd2e00e2500211fa542632322ddd11f6b
floratoth <a.flora95@gmail.com>
2022-10-05 15:17:55 +0200

    Add getters to login and add checkLoginData logic

--- center-div/src/app/features/login/login.component.ts
+++ center-div/src/app/features/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -17,6 +17,14 @@ export class LoginComponent implements OnInit {
   }
 
   ngOnInit(): void {}
+
+  get email(): FormControl {
+    return this.loginForm.get('email') as FormControl;
+  }
+
+  get password(): FormControl {
+    return this.loginForm.get('password') as FormControl;
+  }
 
   login() {
     this.loginForm.setErrors({
--- center-div/src/app/services/authentication.service.ts
+++ center-div/src/app/services/authentication.service.ts
@@ -5,10 +5,17 @@ import { Injectable } from '@angular/core';
 })
 export class AuthenticationService {
   isLogedIn: boolean = false;
+  email: string = "email@email.com";
+  password: string = "password";
 
   constructor() {}
 
   getIsLogedIn() {
     return this.isLogedIn;
   }
+
+  checkLoginData(email: string, password: string): void {
+    this.isLogedIn = (this.email === email && this.password === password);
+  }
+
 }
