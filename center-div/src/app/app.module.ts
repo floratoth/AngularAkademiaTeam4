cd32174c516016926d839d5c5f7d6f55a6fe718a
floratoth <a.flora95@gmail.com>
2022-10-05 13:47:39 +0200

    Add project structure

--- center-div/src/app/app.module.ts
+++ center-div/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './features/home/home.component';
+import { LoginComponent } from './features/login/login.component';
+import { Task1Component } from './features/task1/task1.component';
+import { Task2Component } from './features/task2/task2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    Task1Component,
+    Task2Component
   ],
   imports: [
     BrowserModule,
