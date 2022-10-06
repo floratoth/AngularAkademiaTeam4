a917d80071e170fd6adb963e71063217f99d9953
barianviktor <viktor.barian@gmail.com>
2022-10-06 14:25:07 +0200

    color picker component

--- center-div/src/app/components/stick-note/stick-note.component.ts
+++ center-div/src/app/components/stick-note/stick-note.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { INote } from 'src/app/interfaces/note.interface';
 
 @Component({
   selector: 'app-stick-note',
   templateUrl: './stick-note.component.html',
-  styleUrls: ['./stick-note.component.scss']
+  styleUrls: ['./stick-note.component.scss'],
 })
 export class StickNoteComponent implements OnInit {
+  @Input() note!: INote;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
