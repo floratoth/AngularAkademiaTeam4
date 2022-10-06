a306c5d968a8d9b9b4b4475ee9bb69b08c5d2b0e
Freetze <babos1221@gmail.com>
2022-10-06 13:44:48 +0200

    create interface modify creational conponents

--- center-div/src/app/components/creational-form/creational-form.component.ts
+++ center-div/src/app/components/creational-form/creational-form.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-creational-form',
   templateUrl: './creational-form.component.html',
-  styleUrls: ['./creational-form.component.scss']
+  styleUrls: ['./creational-form.component.scss'],
 })
 export class CreationalFormComponent implements OnInit {
+  @Input() placeHolder?: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
---
+++ center-div/src/app/interfaces/note.interface.ts
@@ -0,0 +1,5 @@
+export interface INote {
+  id?: number
+  content: string
+  create_at: string
+}
