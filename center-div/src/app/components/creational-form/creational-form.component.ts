ec0b6c7b1493891a2b5b939f687611d1da878f93
Freetze <babos1221@gmail.com>
2022-10-06 14:38:15 +0200

    Form editing, validations

--- center-div/src/app/components/creational-form/creational-form.component.ts
+++ center-div/src/app/components/creational-form/creational-form.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-creational-form',
@@ -6,9 +12,24 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./creational-form.component.scss'],
 })
 export class CreationalFormComponent implements OnInit {
-  @Input() placeHolder?: string;
+  noteForm: FormGroup;
 
-  constructor() {}
+  constructor(private fb: FormBuilder) {
+    this.noteForm = fb.group({
+      content: ['', [Validators.required]],
+      color: ['', [Validators.required]],
+    });
+  }
 
+  get content(): FormControl {
+    return this.noteForm.get('content') as FormControl;
+  }
+  get color(): FormControl {
+    return this.noteForm.get('color') as FormControl;
+  }
+
+  handleSubmit() {
+    console.log(this.noteForm);
+  }
   ngOnInit(): void {}
 }
